d2685e8d43f46a3942d6f2536f352cea1971b845
NtinosVyssino <kostastz2004@gmail.com>
2025-04-07 00:11:10 +0300

    New updated code for Ultra Sonic Sensor

--- ultrasonice.ino
+++ ultrasonice.ino
@@ -2,27 +2,18 @@
 #include <LiquidCrystal_I2C.h>
 
 // Ultrasonic sensor pins
-const int trigPin = 10;
-const int echoPin = 11;
+const int trigPin = 9;
+const int echoPin = 10;
 
-const int BUZZER = 9;
-
-// LED pins
-const int L1 = 2;
-const int L2 = 3;
-const int L3 = 4;
-const int L4 = 5;
-const int L5 = 6;
-
-int leds[] = {L1, L2, L3, L4, L5};
+const int BUZZER = 11;
 
 // Variables for distance measurement
 long duration;
+double fake_distance;
 double distance;
-int numLeds;
 unsigned long startMillis;
 
-const int numberOfSamples = 30;
+const int numberOfSamples = 31; // big data set and odd number to determine the MEDIAN
 double distance_values[numberOfSamples];
 
 // Set the LCD I2C address (0x27 or 0x3F)
@@ -33,56 +24,38 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-  // Initialize LED pins
-  for (int i = 0; i < 5; i++) {
-    pinMode(leds[i], OUTPUT);
-  }
-
   pinMode(BUZZER, OUTPUT);
 
   lcd.init();         // Initialize the LCD
   lcd.backlight();    // Turn on the backlight
   lcd.setCursor(0, 0);
 
-
   startMillis = millis();
-
-  Serial.begin(9600);
 }
 
 void loop() {
 
-  distance = getDistance();
+  fake_distance = getDistance();
+  distance = getMedian();
+  distance = sqrt(sq(distance) - sq(4));
 
-  // Map the distance to LED count and beep speed
-  numLeds = map(distance, 2, 30, 1, 5);
-  numLeds = constrain(numLeds, 0, 5);
-  unsigned long beepInterval = map(distance, 5, 50, 500, 4000);
-
-  
-  // Turn on LEDs depending on distance
-  for (int i = 0; i < 5; i++) {
-    digitalWrite(leds[i], (i < numLeds) ? HIGH : LOW);
-  }
+  unsigned long beepInterval = map(distance, 5, 50, 500, 3000); //Closer = higher pitch
 
   // Buzzer beep
   if (millis() - startMillis >= beepInterval) {
     startMillis = millis();
     tone(BUZZER, 1000, 50); // 50ms beep
-    
-    // Print distance
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.print(" cm - LEDs: ");
-  Serial.println(numLeds);
   
-  lcd.setCursor(0, 0);
-  lcd.print("Dist: ");
-  lcd.print((double)distance, 2);
-  lcd.print(" cm    ");
+  //LCD logic
+  static double previousDistance = -1;
+  if (abs(distance - previousDistance ) > 0.1){
+    previousDistance = distance;
+    lcd.setCursor(0, 0);
+    lcd.print("Dist: ");
+    lcd.print((double)distance, 2);
+    lcd.print(" cm    ");
   }
-
-  
+  }
 
   delay(25);
 }
@@ -91,7 +64,7 @@ double getDistance() {
 
   double sum = 0;
 
-  for (int i = 0; i < numberOfSamples ; i ++) {
+  for (int i = 0; i < numberOfSamples; i++) {
       // Send ultrasonic pulse
       digitalWrite(trigPin, LOW);
       delayMicroseconds(2);
@@ -100,7 +73,7 @@ double getDistance() {
       digitalWrite(trigPin, LOW);
 
       // Measure distance
-      duration = pulseIn(echoPin, HIGH, 25000); //25ms time-out to avoid long waits
+      duration = pulseIn(echoPin, HIGH, 25000); // 25ms time-out to avoid long waits
       distance = duration * 0.034 / 2;
       if (distance > 2 && distance < 400) {
         distance_values[i] = distance;
@@ -112,5 +85,44 @@ double getDistance() {
     }
     
     return sum / numberOfSamples;
+}
 
+double getMedian() {
+
+  // Quick Sort - Used to sort the dataset
+  quicksort(distance_values, 0, numberOfSamples - 1);
+
+  // Find the Median
+  return distance_values[(int)(numberOfSamples / 2)];
+}
+
+// Quick Sort function
+void quicksort(double arr[], int low, int high) {
+  if (low < high) {
+    int pi = partition(arr, low, high); //pi = pivot
+    quicksort(arr, low, pi - 1);
+    quicksort(arr, pi + 1, high);
+  }
+}
+
+// Partition function for Quick Sort
+int partition(double arr[], int low, int high) {
+  double pivot = arr[high];  // pivot element
+  int i = (low - 1);  // index of smaller element
+  
+  for (int j = low; j < high; j++) {
+    if (arr[j] <= pivot) {
+      i++;
+      swap(&arr[i], &arr[j]);
+    }
+  }
+  swap(&arr[i + 1], &arr[high]);
+  return (i + 1);
+}
+
+// Swap function to exchange values using pointers
+void swap(double* a, double* b) {
+  double temp = *a;
+  *a = *b;
+  *b = temp;
 }
